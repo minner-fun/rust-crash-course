9dea5ea7714ee00475fb13c98317adbc53c43bd7
Minner-ubuntu <minner.fun@gmail.com>
2026-05-02 10:18:32 +0800

    var

---
+++ my_projects/hello_rust/examples/hello.rs
@@ -0,0 +1,10 @@
+// 在 examples/hello.rs 中
+#![allow(unused)]
+// main() 是 Rust 程序的入口点
+
+// Rust 中的宏在编译时生成代码，并且是
+// 使用感叹号 (!) 调用。
+fn  main () {
+    // println! 是一个将文本打印到控制台的宏。
+    println! ( "你好，世界！" );
+}
--- topics/hello/exercises/src/main.rs
+++ topics/hello/exercises/src/main.rs
@@ -8,3 +8,30 @@
 // println! is a macro that prints text to the console.
 // Macros in Rust generate code at compile time and are
 // invoked with an exclamation mark (!).
+const NUM: u32 = 123;
+fn main(){
+    println!("hello Rust!");
+    let x = 12;
+    println!("x is {x}");
+    const Y: i32 = 1;
+    let z : i32 = Y + x; 
+    println!("Y is {}", Y);
+    println!("NUM is {NUM}");
+    println!("{0} + {1} = {2}", Y, x, z );
+    
+    let mut name = "Minner";
+    name = "minmin";
+    println!("name is {name}");
+
+    let point = (3,4);
+    println!("DEBUG tuple: point is {:?}", point);
+    println!("DEBUG pretty tuple: point is {:#?}", point);
+
+    let age: _ = 30;
+    println!("age is {age}");
+    let age: u32 = 50;
+    println!("age is {age}");
+    let age: bool = true;
+    println!("age is {age}");
+
+}
