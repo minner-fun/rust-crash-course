924f5b1e7df84536bf3b903d188c118651bab53a
Minner-ubuntu <minner.fun@gmail.com>
2026-05-04 15:52:26 +0800

    for loop

--- topics/for_loop/exercises/src/lib.rs
+++ topics/for_loop/exercises/src/lib.rs
@@ -1,7 +1,11 @@
 pub fn sum(nums: Vec<i32>) -> i32 {
-    todo!();
+    let mut sum: i32 = 0;
+    for num in nums.iter() {
+        sum += num;
+    }
+    return sum;
 }
 
 pub fn fill(i: u32, n: usize) -> Vec<u32> {
-    todo!();
+    vec![i; n]
 }
